295af8170e8b900bb3e7b5caa78d6709b7ac889c
leexsh <851001285@qq.com>
2025-12-08 22:57:12 +0800

    sql

--- product/src/test/java/com/atguigu/product/ProductApplicationTests.java
+++ product/src/test/java/com/atguigu/product/ProductApplicationTests.java
@@ -1,13 +1,21 @@
 package com.atguigu.product;
 
+import com.atguigu.product.generator.domain.Brand;
+import com.atguigu.product.generator.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductApplicationTests {
-
+    @Autowired
+    private BrandService brandService;
     @Test
     void contextLoads() {
+        Brand brand = new Brand();
+        brand.setName("华为");
+        brandService.save(brand);
+        System.out.println("save success");
     }
 
 }
